d7f0352c21529c724a2bb73cbb0f546c6bcef4f4
Sarabpreet S <Sarabpreetsingh23@gmail.com>
2015-11-03 00:26:59 +0530

    - code

---
+++ test.php
@@ -0,0 +1,58 @@
+ <?php
+    function buildBaseString($baseURI, $method, $params) {
+        $r = array();
+        ksort($params);
+        foreach($params as $key=>$value){
+            $r[] = "$key=" . rawurlencode($value);
+        }
+        return $method."&" . rawurlencode($baseURI) . '&' . rawurlencode(implode('&', $r));
+    }
+
+    function buildAuthorizationHeader($oauth) {
+        $r = 'Authorization: OAuth ';
+        $values = array();
+        foreach($oauth as $key=>$value)
+            $values[] = "$key=\"" . rawurlencode($value) . "\"";
+        $r .= implode(', ', $values);
+        return $r;
+    }
+
+    $url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+
+    $oauth_access_token = "240785139-IeckFIIdokhq71KprJTTPtx2lQX0OrKEUR3Vt8hC";
+    $oauth_access_token_secret = "jRzgnsJvkA7Z2QPF1u1NvGUqCy7ONTAmM45VjROLloKcI";
+    $consumer_key = "240785139-IeckFIIdokhq71KprJTTPtx2lQX0OrKEUR3Vt8hC";
+    $consumer_secret = "JhE7YQ0qJhvIOllElsfR6l9fTkUUCPwFMjNW5xceGvZclEotak";
+
+    $oauth = array( 'oauth_consumer_key' => $consumer_key,
+                    'oauth_nonce' => time(),
+                    'oauth_signature_method' => 'HMAC-SHA1',
+                    'oauth_token' => $oauth_access_token,
+                    'oauth_timestamp' => time(),
+                    'oauth_version' => '1.0');
+
+    $base_info = buildBaseString($url, 'GET', $oauth);
+    $composite_key = rawurlencode($consumer_secret) . '&' . rawurlencode($oauth_access_token_secret);
+    $oauth_signature = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true));
+    $oauth['oauth_signature'] = $oauth_signature;
+
+    // Make requests
+    $header = array(buildAuthorizationHeader($oauth), 'Expect:');
+    $options = array( CURLOPT_HTTPHEADER => $header,
+                      //CURLOPT_POSTFIELDS => $postfields,
+                      CURLOPT_HEADER => false,
+                      CURLOPT_URL => $url,
+                      CURLOPT_RETURNTRANSFER => true,
+                      CURLOPT_SSL_VERIFYPEER => false);
+
+    $feed = curl_init();
+    curl_setopt_array($feed, $options);
+    $json = curl_exec($feed);
+    curl_close($feed);
+
+    $twitter_data = json_decode($json);
+
+//print it out
+// print_r ($twitter_data);
+
+?>
