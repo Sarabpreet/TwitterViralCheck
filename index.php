d4732aa9c6f8f7ee30bac211cd8b20e884c5c896
Sarabpreet S <Sarabpreetsingh23@gmail.com>
2015-11-05 15:53:42 +0530

    - basic setup -

--- index.php
+++ index.php
@@ -1,11 +1,40 @@
 <?php include 'header.php' ?>
-<form action="process.php" method="get">
+
+
+    <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation"><a href="#">About</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">Twitter Viral Check Tool</h3>
+      </div>
+
+      <div class="jumbotron row">
+        <h1>Jumbotron heading</h1>
+        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <form action="process.php" method="get" class="col-lg-4">
   
 <legend>Enter things you'd like to search </legend><input type="text" name='query'>
 <legend>Limit the posts to : </legend><input type="text" name='limit'><br>
-<input type="submit">
 
+<input type="submit" class="btn btn-lg btn-success" role="button" value="Search">
 
 
 </form>
+
+      </div>
+
+
+      <footer class="footer">
+        <p>&copy; Built at Vit University by Sarabpreet | Annu | Premjeet </p>
+      </footer>
+
+    </div> <!-- /container -->
+
+
+
+
+
 <?php include 'footer.php' ?>
--- process.php
+++ process.php
@@ -1,4 +1,7 @@
-<?php include 'header.php' ?>
+<?php 
+require "twitteroauth/autoload.php";
+use Abraham\TwitterOAuth\TwitterOAuth;
+include 'header.php' ?>
 
 <?php if(
 
@@ -6,14 +9,52 @@
 	isset($_GET['query'])
 
 	 && isset($_GET['limit']) )  {
+ 
+	$uquery=$_GET['query'];
+	$ulimit=$_GET['limit'];
+	$count=0;
+	
 
-	$query=$_GET['query'];
-	$limit=$_GET['limit'];
-	echo $query.$limit;
+define('CONSUMER_KEY', 'Zy5Ji3EUig67v8uzyrJmxY4iu');
+define('CONSUMER_SECRET', 'JhE7YQ0qJhvIOllElsfR6l9fTkUUCPwFMjNW5xceGvZclEotak');
+define('ACCESS_TOKEN', '240785139-IeckFIIdokhq71KprJTTPtx2lQX0OrKEUR3Vt8hC');
+define('ACCESS_TOKEN_SECRET', 'jRzgnsJvkA7Z2QPF1u1NvGUqCy7ONTAmM45VjROLloKcI');
+ 
+function search(array $query)
+{
+  $toa = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+  return $toa->get('search/tweets', $query);
+}
+    
+$query = array(
+  "q" => $uquery,
+);
+  
+$results = search($query);
+  
+foreach ($results->statuses as $result) {
+
+	$count++;
+
+	if($count<=$ulimit) {
+
+		  echo "<div class='box'>".$result->user->screen_name . ": " . $result->text ." Posted on".$result->created_at. "</div>";
+
+	}
+}
 
 
 
-	} ?>
+	} 
+// if ends here... 
+
+	else {
+
+
+
+
+	}
+	?>
 
 
 
